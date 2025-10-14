b7d8d4a0e0ccf9daee03189c53396d6f487f9085
wjnaby <darwishzafril@gmail.com>
2025-10-14 15:28:54 +0800

    Remove vendor/bundle from repo

--- vendor/bundle/ruby/3.2.0/gems/bootstrap-5.3.5/lib/bootstrap/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Bootstrap
-  VERSION       = '5.3.5'
-  BOOTSTRAP_SHA = '85f23534bd2de8041354b297516cf21959091b31'
-end
--- vendor/bundle/ruby/3.2.0/gems/wicked_pdf-2.8.2/lib/wicked_pdf/version.rb
+++
@@ -1,3 +0,0 @@
-class WickedPdf
-  VERSION = '2.8.2'.freeze
-end
